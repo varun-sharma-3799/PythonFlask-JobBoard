e1cb7b9f9ef8a1e85719ac35a0ca41329218909f
Varun Sharma <varunsharma3799@gmail.com>
2022-09-26 18:55:57 +0530

    adding "_macro"

--- jobs/app.py
+++ jobs/app.py
@@ -1,8 +1,45 @@
-from flask import Flask, render_template
+# from flask import Flask, render_template
+
+# app = Flask(__name__)
+
+# @app.route('/')
+# @app.route('/jobs')
+# def jobs():
+#   return render_template('index.html')
+
+import sqlite3
+from flask import Flask, render_template, g
+
+PATH = 'db/jobs.sqlite'
 
 app = Flask(__name__)
+
+def open_connection():
+    connection = getattr(g, '_connection', None)
+    if connection == None:
+        connection = g._connection = sqlite3.connect(PATH)
+    connection.row_factory = sqlite3.Row
+    return connection
+
+def execute_sql(sql, values=(), commit=False, single=False):
+    connection = open_connection()
+    cursor = connection.execute(sql, values)
+    if commit == True:
+        results = connection.commit()
+    else:
+        results = cursor.fetchone() if single else cursor.fetchall()
+
+    cursor.close()
+    return results
+
+@app.teardown_appcontext
+def close_connection(exception):
+    connection = getattr(g, '_connection', None)
+    if connection is not None:
+        connection.close()
 
 @app.route('/')
 @app.route('/jobs')
 def jobs():
-  return render_template('index.html')
+    jobs = execute_sql('SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id')
+    return render_template('index.html', jobs=jobs)
